5c432c04d97faa54f165ecfccd13a326248db728
zengy <1635942631@qq.com>
2018-12-29 16:57:59 +0800

    4

--- Application/Web/Controller/ApiController.class.php
+++ Application/Web/Controller/ApiController.class.php
@@ -41,7 +41,9 @@ class ApiController extends CommonController
     	$this->attend();
 		//将提交的json格式转换并保存
 		$all_data = json_encode($_POST);
-		// var_dump($data_json);exit;
+		var_dump($all_data);
+		echo "<br>222222"
+		var_dump($data_json);exit;
 		//将获取的内容插入数据库cs_content
 		// $aa = M('content')->add($data_json);
 		// if(!empty($aa)){
