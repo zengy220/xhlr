38c9f3657ecc7e7668e9fedd7a50fbba37a9aa31
zengy <1635942631@qq.com>
2019-01-09 09:31:03 +0800

    9

--- Application/Web/Controller/ApiController.class.php
+++ Application/Web/Controller/ApiController.class.php
@@ -6,27 +6,77 @@ class ApiController extends CommonController
 {
 
 	 public function sms(){
-//        echo 2;
 
-        $phone = I('phone');
-        $rand = rand(1000,9999);
-        // $url = "http://api.chanyoo.cn/utf8/interface/send_sms.aspx?username=guomengtao1&password=aa77bb&content=验证码：".$rand."【高血压】&receiver=".$phone;
-        $url = "http://118.178.86.197/cmas/cmasoutapi.do?method=httpSend&username=hnxjw&password=874639&mobile=".$phone."&msg=验证码是".$rand."&sign=bf944381d72d1f05f5d6fe68f8070645&needstatus=true&needmo=false";
+	 	header("Content-Type:text/html;charset=utf-8");
+ 
+		//账号
+		$username= "hnxjw";
+		 
+		//验证码
+		$rand = rand(1000,9999);
+		//密码
+		$pswd =  "874639";
+		 
+		//修改为您要发送的手机号
+		$mobile = I('phone');
+		 
+		//发送地址，请咨询客服
+		$url = "http://118.178.86.197/cmas/cmasoutapi.do?method=httpSend&username=hntest&password=1234&mobile=13800138000&msg=httptest&sign=520e890c77010ff61b2&needstatus=true&needmo=false";
+		 
+		//是否需要状态报告,为true即可
+		$needstatus = "true"; 
+		 
+		//产品，为空即可
+		$product = ""; 
+		 
+		$ch = curl_init();
+		 
+		/* 设置验证方式 */
+		curl_setopt($ch, CURLOPT_HTTPHEADER, 
+		array('Accept:text/plain;charset=utf-8', 'Content-Type:application/x-www-form-urlencoded','charset=utf-8'));
+		 
+		/* 设置返回结果为流 */
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		 
+		/* 设置超时时间*/
+		curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+		 
+		/* 设置通信方式 */
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		 
+		 
+		// 发送短信
+		$msg="【科技】您的验证码是：".$rand;  
+		$data=array('msg'=>$msg,'username'=>$username,'pswd'=>$pswd,'mobile'=>
+		$mobile,'needstatus'=>$needstatus,'product'=>$product);
+		$result = send($ch,$data);
+		echo '<pre>';print_r($result);
+		 
+		curl_close($ch);
+		exit;
+		 
+		/***************************************************************************************/
+		 
+		 
 
-        // var_dump($url);exit;
 
-        $file = file_get_contents($url);
-        // $file = file_get_contents("https://www.baidu.com");
-        var_dump($file);exit;
-        
+
+
+
+
 
         // $phone = I('phone');
         // $rand = rand(1000,9999);
-        // $url = "http://api.chanyoo.cn/utf8/interface/send_sms.aspx?username=guomengtao1&password=aa77bb&content=验证码：".$rand."【高血压】&receiver=".$phone;
+        // // $url = "http://api.chanyoo.cn/utf8/interface/send_sms.aspx?username=guomengtao1&password=aa77bb&content=验证码：".$rand."【高血压】&receiver=".$phone;
+        // $url = "http://118.178.86.197/cmas/cmasoutapi.do?method=httpSend&username=hnxjw&password=874639&mobile=".$phone."&msg=验证码是".$rand."&sign=bf944381d72d1f05f5d6fe68f8070645&needstatus=true&needmo=false";
 
+        // // var_dump($url);exit;
 
         // $file = file_get_contents($url);
+        // // $file = file_get_contents("https://www.baidu.com");
         // var_dump($file);exit;
+        
 
 
 
@@ -387,6 +437,14 @@ class ApiController extends CommonController
 	 $search = array(" ","　","\n","\r","\t");
 	 $replace = array("","","","","");
 	 return str_replace($search, $replace, $str);
+	}
+
+
+	//验证码
+	function send($ch,$data){
+	    curl_setopt ($ch, CURLOPT_URL, $url);
+	    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+	    return curl_exec($ch);
 	}
 
 
